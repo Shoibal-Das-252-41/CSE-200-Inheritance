0a45cd48a26dd89eb2fa0259862cfbae0ef922c9
Shoibal-Das-252-41 <shoibalshantu@gmail.com>
2021-01-19 21:44:22 +0600

    first commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        class Car
+        {
+            public string brand;
+            public string color;
+            public int price;
+
+            public void DisplyaCarInfo()
+            {
+                Console.WriteLine("Brand : " + brand);
+                Console.WriteLine("Color : " + color);
+                Console.WriteLine("Price : " + price);
+            }
+        }
+
+        class RangeRover : Car
+        {
+            float mileage;
+
+            void DisplayMileage()
+            {
+                Console.WriteLine("Mileage : " + mileage);
+            }
+
+            static void Main(string[] args)
+            {
+
+                RangeRover myCar = new RangeRover();
+                myCar.brand = "Range Rover";
+                myCar.color = "Desert";
+                myCar.price = 20000000;
+                myCar.mileage = 34.7f;
+                myCar.DisplyaCarInfo();
+                myCar.DisplayMileage();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
